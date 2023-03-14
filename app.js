48823644fdab8c13b28cf9a98d473b8b13229701
James <jamesnjorio@gmail.com>
2023-03-14 16:21:10 +0300

    Added axios, dotenv,express libraries and created server file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const puzzleBoard = document.querySelector("#puzzle");
 const solveButton = document.querySelector("#solve-button");
+const solutionDisplay = document.querySelector("#solution")
 
 const squares = 81;
 const submission = [];
@@ -9,6 +10,15 @@ for (let i = 0; i < squares; i++) {
   inputElement.setAttribute("type", "number");
   inputElement.setAttribute("min", "0");
   inputElement.setAttribute("max", "9");
+  if(
+    ((1 % 9 == 0 || i % 9 == 2) && i < 21) ||
+    ((1 % 9 == 6 || i % 9 == 8) && i < 27) ||
+    ((1 % 9 == 3 || i % 9 == 5) && (i > 27 &&  i < 53)) ||
+    ((1 % 9 == 0 || i % 9 == 2) && i < 53) ||
+    ((1 % 9 == 6 || i % 9 == 8) && i < 53) 
+  ){
+    inputElement.classList.add('odd-section')
+  }
   puzzleBoard.appendChild(inputElement);
 }
 
@@ -22,25 +32,42 @@ const joinValues = () => {
     }
   });
 
-//   console.log(submission);
+    console.log(submission);
+};
+
+const populateValues = (isSolvable) => {
+    const inputs = document.querySelectorAll('input');
+    if(isSolvable && solution){
+        inputs.forEach((input,i) => {
+            input.value = solution[i];
+        });
+        solutionDisplay.innerHTML = "This is the answer"
+    }else{
+        solutionDisplay.innerHTML = "This is not solvable"
+
+    }
 };
 
 const solve = () => {
+  joinValues();
+  const data = submission.join('');
+  console.log("data", data);
   const options = {
     method: "POST",
     url: "https://sudoku-solver3.p.rapidapi.com/sudokusolver/",
     headers: {
       "content-type": "application/json",
-      "X-RapidAPI-Key": "f1857422bbmsh0752a21769acc23p130f0djsn11ceb86b05b9",
+      "X-RapidAPI-Key": process.env.SUDOKU_API_KEY,
       "X-RapidAPI-Host": "sudoku-solver3.p.rapidapi.com",
     },
-    data: '{"input":[0,0,8,9,0,0,4,0,0,0,0,5,6,0,0,0,0,0,3,0,0,7,0,0,6,0,9,5,0,0,0,0,4,0,2,0,0,0,0,0,0,0,0,6,5,0,0,2,0,0,1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,8,9,0,0,0,7,0,4,0,0,2,0,0]}',
+    data:data,
   };
 
   axios
     .request(options)
     .then(function (response) {
       console.log(response.data);
+      populateValues(response.data.solvable, response.data.solution)
     })
     .catch(function (error) {
       console.error(error);
