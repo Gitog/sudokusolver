7ccd4adebf45dd3c5c6abb8dfa7228ccc6b14354
James <jamesnjorio@gmail.com>
2023-03-14 08:27:56 +0300

    Added css to Style

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const solveButton= document.querySelector('#solve-button')
 
 const squares = 81
 
-for (let i = 0; i <squares; i++){
+for (let i = 0; i < squares; i++){
     const inputElement = document.createElement('input')
     inputElement.setAttribute('type', 'number')
     inputElement.setAttribute('min', '0')
